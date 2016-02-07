0fd33a03e40b04cf1e3432eebd35a769f1ca62c0
Jaime Aranaz <jaime@corpora360.com>
2016-02-07 17:34:24 +0100

    NAP-3 Declare method for sending requests

---
+++ NumbersAppi/NumbersAppi/ApiServices.swift
@@ -0,0 +1,17 @@
+//
+//  ApiServices.swift
+//  NumbersAppi
+//
+//  Created by Jaime on 07/02/16.
+//  Copyright © 2016 Jaime Aranaz. All rights reserved.
+//
+
+import Foundation
+
+class ApiServices {
+    
+    func sendRequest(request: ApiRequest, completion: (response: ApiResponse, error: NSError) -> Void) {
+        
+    }
+
+}
