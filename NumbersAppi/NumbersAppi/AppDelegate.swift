4f17d2cf18eac88caa4617a946a7aa7970af224b
Jaime Aranaz <jaime@corpora360.com>
2016-02-14 17:50:45 +0100

    NAP-4 Load main xib from app delegate, and create basic groups in project

--- NumbersAppi/NumbersAppi/AppDelegate.swift
+++ NumbersAppi/NumbersAppi/AppDelegate.swift
@@ -15,19 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        let request = ApiRequest()
-        
-        request.type = ApiRequestType.Year
-        request.number = -1234
-        
-        ApiServices.sharedInstance.sendRequest(request, completion: { (response, error) -> Void in
-
-            if (error != nil) {
-                NSLog("response failed")
-            } else {
-                NSLog("response successful")
-            }
-        })
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        if let window = window {
+            window.backgroundColor = UIColor.whiteColor()
+            window.rootViewController = MainViewController()
+            window.makeKeyAndVisible()
+        }
         
         return true
     }
