21355084de26fd9bdb2382d23dcc0fa1ebf6054a
Jaime Aranaz <jaime@corpora360.com>
2016-02-14 17:57:29 +0100

    NAP-4 Create base classes for Main view controller

---
+++ NumbersAppi/NumbersAppi/AboutViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.swift
+//  NumbersAppi
+//
+//  Created by Jaime Aranaz on 14/02/16.
+//  Copyright © 2016 Jaime Aranaz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+}
---
+++ NumbersAppi/NumbersAppi/MainPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  MainPresenter.swift
+//  NumbersAppi
+//
+//  Created by Jaime Aranaz on 14/02/16.
+//  Copyright © 2016 Jaime Aranaz. All rights reserved.
+//
+
+import Foundation
+
+class MainPresenter: NSObject {
+
+}
