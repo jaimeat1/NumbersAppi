b6bf05a453aee57aea09543cf2bbf361cd50027a
Jaime Aranaz <jaimeat1@gmail.com>
2016-03-24 11:32:06 +0100

    NAP-13 Updating constraints for iPhone, in progress

--- NumbersAppi/NumbersAppi/MainView.swift
+++ NumbersAppi/NumbersAppi/MainView.swift
@@ -27,7 +27,6 @@ class MainView: UIView, MainViewProtocol {
     @IBOutlet var numberPickerView: NumberPickerView!
     @IBOutlet var datePickerView: DatePickerView!
     @IBOutlet var textResult: UILabel!
-    @IBOutlet var doubleTapInfo: UILabel!
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
     
     var delegate: MainViewDelegate!
@@ -48,8 +47,6 @@ class MainView: UIView, MainViewProtocol {
         setupTypeSelector()
         setupTextResult()
         addTapGestures()
-        
-        doubleTapInfo.text = NSLocalizedString("DOUBLE_TAP", comment: "Info message for double tap and random number")
     }
     
     // MARK: - Action methods
