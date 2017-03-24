b57d28d4fb9a0eec406248d58ba6b8598552d188
Jaime Aranaz <jaimeat1@gmail.com>
2017-03-24 21:51:15 +0100

    Change project’s folder

--- NumbersAppi/Pods/Target Support Files/NSTimer-Blocks/NSTimer-Blocks-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "NSTimer+Blocks.h"
-
-FOUNDATION_EXPORT double NSTimer_BlocksVersionNumber;
-FOUNDATION_EXPORT const unsigned char NSTimer_BlocksVersionString[];
-
